479b8e0b320c999a3fd4af7f4cad12a694d7e409
Lukasz Kurek <lukascoorek@gmail.com>
2019-06-30 21:39:47 +0200

    adding worning modal

--- main.js
+++ main.js
@@ -6,12 +6,9 @@ function init() {
   const add = document.querySelector("#add");
   const remove = document.querySelector("#remove");
   const clear = document.querySelector("#clear");
+  const container = document.querySelector(".container");
   const list = document.querySelector("ul.list-group");
-  let tasks = [
-    { task: "Task number", id: 1 },
-    { task: "Task number", id: 2 },
-    { task: "Task number", id: 3 }
-  ];
+  let tasks = [];
   function render(tasks) {
     let innerHtml = "";
     if (tasks.length > 0) {
@@ -29,18 +26,37 @@ function init() {
     }
     list.innerHTML = innerHtml;
   }
-
   render(tasks);
+  function warning(container) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "alert alert-dismissible alert-warning");
+    div.innerHTML = `
+    <p class="mb-0">No tasks</p>
+    <p class="mb-0">Press Add button</p>
+  `;
+    container.appendChild(div);
+    setTimeout(() => {
+      container.removeChild(div);
+    }, 3000);
+  }
   add.addEventListener("click", () => {
     tasks = [...tasks, { task: "Task number", id: tasks.length + 1 }];
     render(tasks);
   });
   remove.addEventListener("click", () => {
-    tasks.pop();
-    render(tasks);
+    if (tasks.length > 0) {
+      tasks.pop();
+      render(tasks);
+    } else {
+      warning(container);
+    }
   });
   clear.addEventListener("click", () => {
-    tasks = [];
-    render(tasks);
+    if (tasks.length > 0) {
+      tasks = [];
+      render(tasks);
+    } else {
+      warning(container);
+    }
   });
 }
